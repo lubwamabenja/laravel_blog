84e36cfbbacf528847a3304c11a00509786b11b9
lubwamabenja <lubwama73@gmail.com>
2020-04-29 15:42:48 -0700

    verify email

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,10 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['verify' =>'true']);
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
+Auth::routes(['verify' =>'true']);
 
 Route::get('/home', 'HomeController@index')->name('home');
